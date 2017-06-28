eb7f6ec898f103ee55b9c880ded964c1043a9eeb
siemens279 <siemens279@gmail.com>
2017-06-28 14:05:16 +0300

    1405-2806

--- app/src/main/java/com/siemens/diary/MainActivity.java
+++ app/src/main/java/com/siemens/diary/MainActivity.java
@@ -1,13 +1,37 @@
 package com.siemens.diary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mExit:
+                        mAuth.signOut();
+                        startActivity(new Intent(MainActivity.this, SingInActivity.class));
+                        break;
+                }
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
+
 }
--- app/src/main/java/com/siemens/diary/fragments/f_registration.java
+++ app/src/main/java/com/siemens/diary/fragments/f_registration.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +23,7 @@ public class f_registration extends Fragment {
 
     private FirebaseAuth mAuth;
     EditText etEmail, etPassword, etNane, etAge;
-    Button bRegistration;
+    Button bRegistration, bBack;
 
     @Nullable
     @Override
@@ -38,6 +39,21 @@ public class f_registration extends Fragment {
             @Override
             public void onClick(View view) {
                 registration(etEmail.getText().toString(), etPassword.getText().toString());
+            }
+        });
+        bBack = (Button) view.findViewById(R.id.buttonBack);
+        bBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = null;
+                Class fragmentClass = f_sing_in.class;
+                FragmentManager fragmentManager = getFragmentManager();
+                try {
+                    fragment = (Fragment) fragmentClass.newInstance();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
             }
         });
         return view;
