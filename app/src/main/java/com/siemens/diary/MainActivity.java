736b6e3111efd38dfd127f062f979f31a28d5050
siemens279 <siemens279@gmail.com>
2017-06-28 16:25:53 +0300

    1625-2806

--- app/src/main/java/com/siemens/diary/MainActivity.java
+++ app/src/main/java/com/siemens/diary/MainActivity.java
@@ -1,6 +1,7 @@
 package com.siemens.diary;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,12 +13,18 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     private FirebaseAuth mAuth;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        initToolbar();
+        initNavigationView();
+    }
+
+    private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
@@ -32,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
     }
 
 }
